8baecf170b03fe06f3b5e36502361e9b5d028231
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2026-04-27 17:31:04 -0700

    generation of list_resources

--- google/services/cloudrun/resource_cloud_run_service_sweeper.go
+++ google/services/cloudrun/resource_cloud_run_service_sweeper.go
@@ -144,9 +144,9 @@ func listAndActionCloudRunService(action sweeper.ResourceAction) error {
 		}
 
 		// First try the expected resource key
-		resourceList, ok := res["services"]
+		resourceList, ok := res["items"]
 		if ok {
-			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'services'")
+			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'items'")
 		} else {
 			// Next, try the common "items" pattern
 			resourceList, ok = res["items"]
