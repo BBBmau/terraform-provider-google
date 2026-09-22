3baebf1b0a849831c7d0c8735895aad0d3f6eef8
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2026-09-22 16:36:46 -0700

    Schedule focused nightly tests in four minutes

--- .teamcity/components/projects/google_ga_subproject.kt
+++ .teamcity/components/projects/google_ga_subproject.kt
@@ -31,7 +31,7 @@ fun googleSubProjectGa(allConfig: AllContextParameters): Project {
         description = "Subproject containing builds for testing the GA version of the Google provider"
 
         // Nightly Test project that uses hashicorp/terraform-provider-google
-        subProject(nightlyTests(gaId, ProviderNameGa, HashiCorpVCSRootGa, gaConfig, NightlyTriggerConfiguration(startHour = 23, startMinute = 12)))
+        subProject(nightlyTests(gaId, ProviderNameGa, HashiCorpVCSRootGa, gaConfig, NightlyTriggerConfiguration(startHour = 16, startMinute = 40)))
 
         // MM Upstream project that uses modular-magician/terraform-provider-google
         subProject(mmUpstream(gaId, ProviderNameGa, ModularMagicianVCSRootGa, HashiCorpVCSRootGa, vcrConfig, NightlyTriggerConfiguration()))
